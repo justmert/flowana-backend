6f77162ccc98b4bbd6cd7b49748660e94abef8c6
Mert Köklü <kklumert@gmail.com>
2023-08-07 12:45:05 +0300

    fixes

--- pipeline.py
+++ pipeline.py
@@ -238,13 +238,15 @@ class Pipeline:
         )
 
     def run_pipelines(self):
-        self.run_project_pipeline(PipelineType.GITHUB_PROJECTS, self.project_pipeline_functions)
-        self.run_protocol_pipeline(PipelineType.GITHUB_CUMULATIVE, self.protocol_github_functions)
-        self.run_protocol_pipeline(PipelineType.GITHUB_LEADERBOARD, self.protocol_leaderboard_functions)
+
         self.run_protocol_pipeline(PipelineType.DISCOURSE, self.protocol_discourse_functions)
         self.run_protocol_pipeline(PipelineType.DEVELOPERS, self.protocol_developers_functions)
         self.run_protocol_pipeline(PipelineType.GOVERNANCE, self.protocol_governance_functions)
         self.run_protocol_pipeline(PipelineType.MESSARI, self.protocol_messari_functions)
+        self.run_project_pipeline(PipelineType.GITHUB_PROJECTS, self.project_pipeline_functions)
+        self.run_protocol_pipeline(PipelineType.GITHUB_CUMULATIVE, self.protocol_github_functions)
+        self.run_protocol_pipeline(PipelineType.GITHUB_LEADERBOARD, self.protocol_leaderboard_functions)
+
 
     def function_executer(self, f, *args, **kwargs):
         logging.info(f"[...] Running function: {f.__name__}")
