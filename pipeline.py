8c715bbc33a17ea8850d1dd71942c9f1d0a02dd6
Mert Köklü <kklumert@gmail.com>
2023-08-26 18:32:20 +0300

    fixed

--- pipeline.py
+++ pipeline.py
@@ -221,13 +221,13 @@ class Pipeline:
         )
 
     def run_pipelines(self):
-        self.run_protocol_pipeline(helpers.PipelineType.GOVERNANCE, self.protocol_governance_functions)
-        self.run_protocol_pipeline(helpers.PipelineType.DISCOURSE, self.protocol_discourse_functions)
-        self.run_protocol_pipeline(helpers.PipelineType.DEVELOPERS, self.protocol_developers_functions)
+        # self.run_protocol_pipeline(helpers.PipelineType.GOVERNANCE, self.protocol_governance_functions)
+        # self.run_protocol_pipeline(helpers.PipelineType.DISCOURSE, self.protocol_discourse_functions)
+        # self.run_protocol_pipeline(helpers.PipelineType.DEVELOPERS, self.protocol_developers_functions)
         # self.run_protocol_pipeline(helpers.PipelineType.MESSARI, self.protocol_messari_functions) # will implement later
         self.run_project_pipeline(helpers.PipelineType.GITHUB_PROJECTS, self.project_pipeline_functions)
-        self.run_protocol_pipeline(helpers.PipelineType.GITHUB_CUMULATIVE, self.protocol_github_functions)
-        self.run_protocol_pipeline(helpers.PipelineType.GITHUB_LEADERBOARD, self.protocol_leaderboard_functions)
+        # self.run_protocol_pipeline(helpers.PipelineType.GITHUB_CUMULATIVE, self.protocol_github_functions)
+        # self.run_protocol_pipeline(helpers.PipelineType.GITHUB_LEADERBOARD, self.protocol_leaderboard_functions)
 
     def function_executer(self, f, *args, **kwargs):
         logging.info(f"[...] Running function: {f.__name__}")
